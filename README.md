0816146969cac81afac31ae9141808e20ce79b90
Brandon Wu <brandonmwu888@gmail.com>
2026-01-22 12:36:53 -0800

    Update README with data attribution link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # arc-raiders-helper
+
 Personal webdev exploration project related to the game Arc Raiders
+
+The data comes from [text](https://metaforge.app/arc-raiders)
--- get_data.py
+++ get_data.py
@@ -1,16 +1,19 @@
 """
 Reformat API output. Output json with each map having [condition, start, end].
 """
-from collections import defaultdict
-import requests
-import pathlib
 
+import pathlib
+from collections import defaultdict
+
+import requests
 from pydantic import BaseModel
+
 
 # Read in
 class Times(BaseModel):
     start: str
     end: str
+
 
 class Condition(BaseModel):
     game: str
@@ -21,8 +24,10 @@ class Condition(BaseModel):
     days: list
     times: list[Times]
 
+
 class Data(BaseModel):
     data: list[Condition]
+
 
 # Write out
 class ConditionTimes(BaseModel):
@@ -30,9 +35,11 @@ class ConditionTimes(BaseModel):
     start: str
     end: str
 
+
 class Maps(BaseModel):
     map_name: str
     conditions: list[ConditionTimes]
+
 
 class MapList(BaseModel):
     data: list[Maps]
@@ -43,22 +50,25 @@ def main():
     response = requests.get(url)
     response = response.json()
     m = Data(**response)
-    
+
     maps = defaultdict(list)
     for condition in m.data:
         for time in condition.times:
-            maps[condition.map].append({"name": condition.name, "start": time.start, "end": time.end})
+            maps[condition.map].append(
+                {"name": condition.name, "start": time.start, "end": time.end}
+            )
 
     new_maps = {"data": []}
 
     for map_name in maps:
         maps[map_name].sort(key=lambda x: x["start"])
         new_maps["data"].append({"map_name": map_name, "conditions": maps[map_name]})
-        
+
     new_m = MapList(**new_maps)
     json_string = new_m.model_dump_json(indent=2)
     file_path = pathlib.Path("Map_condition_timestamps.json")
     file_path.write_text(json_string, encoding="utf-8")
+
 
 if __name__ == "__main__":
     main()
